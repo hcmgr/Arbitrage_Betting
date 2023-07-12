81e80089b1abc44540525aabb62bf5891f24e547
hcmgr <harryrackemann@gmail.com>
2023-07-12 15:10:42 +1000

    got paths fixed

--- src/server/server.py
+++ src/server/server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder="../client")
+app = Flask(__name__, template_folder="../client/templates")
 
+# Route to serve the index.html file
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/static/bundle.js')
+def bundle():
+    return send_from_directory('dist', 'bundle.js')
+
+@app.route('/bix')
+def bix():
+    return 'bix'
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(debug=True)
