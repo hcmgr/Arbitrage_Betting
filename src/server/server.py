f482830fb94a183b938b7f70fe874b2b2dde1bc7
hcmgr <harryrackemann@gmail.com>
2023-07-12 11:55:03 +1000

    fixed dir structure again

--- src/server/server.py
+++ src/server/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="../client/templates")
+app = Flask(__name__, template_folder="../client")
 
 @app.route('/')
 def index():
-    return render_template('public/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
