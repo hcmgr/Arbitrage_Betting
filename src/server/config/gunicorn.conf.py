31e54c39e9dd37e76d2f66b5d991c1f2b9f17229
hcmgr <harryrackemann@gmail.com>
2023-07-12 17:35:10 +1000

    incorporated gunicorn

---
+++ src/server/config/gunicorn.conf.py
@@ -0,0 +1,12 @@
+# Number of worker processes
+workers = 4
+
+# Concurrency model
+worker_class = 'sync'
+
+# Maximum request timeout (in seconds)
+timeout = 30
+
+# Socket binding
+bind = '127.0.0.0:8000'
+
---
+++ src/server/wsgi.py
@@ -0,0 +1,7 @@
+from server import app
+
+if __name__ == '__main__':
+    app.run()
+
+# Gunicorn application object
+application = app
