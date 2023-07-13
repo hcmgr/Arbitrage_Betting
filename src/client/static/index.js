531655a90721ed4a669c8ac9fb8754b5ca747e9d
hcmgr <harryrackemann@gmail.com>
2023-07-13 14:54:40 +1000

    fixed bundle.js compilation

--- src/client/static/index.js
+++ src/client/static/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App'
 
 // Render the component into the root element
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById('root')
+// ReactDOM.render(
+//   React.createElement(App, null),
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
 );
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/static/App.js',
-  output: {
-    path: path.resolve(__dirname, 'server/dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-};
+    entry: './client/static/index.js',
+    output: {
+      path: path.resolve(__dirname, 'server/dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
